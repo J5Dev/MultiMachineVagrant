28ad6634f7febb44f55918806accd5219b31a29b
Chris Gooding <chris@thisiseyeweb.co.uk>
2014-05-14 13:14:14 +0100

    Added granting access to root user from any host

--- install_db.sh
+++ install_db.sh
@@ -10,6 +10,11 @@ sudo apt-get update
 sudo apt-get install -y mysql-server mysqltuner
 
 sed -i 's/bind-address.*/bind-address = 0.0.0.0/g' /etc/mysql/my.cnf
+
+mysql -uroot -proot -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root'";
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%'";
+mysql -uroot -proot -e "FLUSH PRIVILEGES";
+
 service mysql restart
 
 echo "--- All done! Would you like to play a game? ---"
