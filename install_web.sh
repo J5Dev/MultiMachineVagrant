101faf295f2a0ed808c5796f68f79511b124a957
Chris Gooding (MBP) <chris.j5dev@gmail.com>
2014-06-08 12:05:47 +0100

    Added support for short open tags in web server php config

--- install_web.sh
+++ install_web.sh
@@ -16,6 +16,7 @@ sudo a2enmod rewrite
 
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sed -i "s/short_open_tag = .*/short_open_tag = On/" /etc/php5/apache2/php.ini
 sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
 
 sudo service apache2 restart
